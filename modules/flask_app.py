f9f6829b397597516e8e6819c8a5dc0d196ca68e
Oleksandra2020 <oleksandrahutor@gmail.com>
2020-05-18 11:26:15 +0300

    Update web app

--- modules/flask_app.py
+++ modules/flask_app.py
@@ -17,7 +17,8 @@ ctx.verify_mode = ssl.CERT_NONE
 def index():
     return render_template('index.html')
 
-@app.route("/results", methods = ['POST'])
+
+@app.route("/results", methods=['POST'])
 def show_search_results():
     variable = request.form['acc']
     search = SearchADT(variable)
@@ -25,63 +26,16 @@ def show_search_results():
     if talks == []:
         return render_template('no_response.html')
     talk = talks[0]
-    result = """
-    <html lang="en">
-    <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width">
-        <style>
-        div {
-            position: absolute;
-            min-height: 975px;
-            min-width:1700px;
-            background: url(https://s17736.pcdn.co/wp-content/uploads/2016/05/shutterstock_161594762.jpg) repeat-x;
-            background-size: cover;
-            }
-        </style>
-    </head>
-    <body>
-    <div>
-        <h1 style="color: white; position: absolute; margin-left: 5%; margin-top: 5%; font-size: px;font-style:inherit;">
-        <form method="post" action="/">
-            <center><input 
-                value="TED ПОШУК" 
-                style="text-align: center; font-size: xx-large; color: brown; margin-bottom: 7 em; margin-left: 2em" 
-                type="submit">
-            </center>
-        </form>
-        <h1 style="color: white; text-align:center; margin-top: 5em; font-size:25px;font-style:inherit;">
-        Результат пошуку: """ + \
-        talk.get_title() + \
-        f"""
-        <a href="{talk.get_ted_link()}" "style="color: white"change hyperlink color>ted link</a>
-        </h1>
-        <h1 style="color: white; text-align:center; font-size:25px;font-style:inherit;">
-        <bottom><iframe width="700" height="455" src=https://www.youtube.com/embed/{talk.get_id()[:-5]} 
-        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </bottom>
-        </h1>
-        <h1  style="color: white; position: absolute; margin-top: 8em; margin-left: 25em; font-size:20px;font-style:inherit;">
-        {talk.get_views()} переглядів. Доступні {search.get_same_translator(talk.get_id())} відео, перекладених цим перекладачем
-        </h1>
-        <h1  style="color: white; position: absolute; margin-top: 1em; margin-left: 10em; margin-right: 10em; text-align:center; font-size:20px;font-style:inherit;">
-        {talk.get_description()}
-        </h1>
-        """
-    i = 22
-    for talk in talks[1:]:
-        result += \
-            f"""
-            <h1  style="color: white; position: absolute; margin-left: 55em; margin-top: 2 em; top: {i}%; font-size:22px;font-style:inherit;">
-            <a href="https://www.youtube.com/watch?v={talk.get_id()[:-5]}"style="color: white"change hyperlink color>{talk.get_title()}</a>
-            """
-        i += 5
-    end = """
-    </div>
-    </body>
-</html>
-"""
-    return result
+    return render_template('main.html', video_title=talk.get_title(),
+                           ted_link=talk.get_ted_link(),
+                           video_id=talk.get_id()[:-5], views=talk.get_views(),
+                           num_translator=search.get_same_translator(
+                               talk.get_id()),
+                           description=talk.get_description(), t=talks[1:])
 
+@app.route("/new_video", methods=['POST'])
+def video_play():
+    variable = request.form['acc']
 
 if __name__ == "__main__":
     app.run(debug=True)
